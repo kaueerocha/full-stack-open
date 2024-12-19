80460e25fa1e12fed1f7a17ef4bd406ff89b33d6
Kauee Rocha Puertas <kauee-rocha@hotmail.com>
2024-12-19 14:46:15 -0300

    add 1.5 courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 export const Header = ( props ) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
@@ -15,7 +15,7 @@ export const Part = (props) => {
 export const Content = (props) => {
   return (
     <div>
-      {props.parts.map((part, index) => (
+      {props.parts.parts.map((part, index) => (
         <Part key={index} name={part.name} exercises={part.exercises} />
       ))}
     </div>
@@ -23,25 +23,36 @@ export const Content = (props) => {
 };
 
 export const Total = ( props ) => {
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
+  const total = props.parts.parts.reduce((sum, part) => sum + part.exercises, 0);
   return (
     <p>{total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Desenvolvimento de aplicação Half Stack'
-  const parts = [
-    { name: 'Fundamentos da biblioteca React', exercises: 10 },
-    { name: 'Usando props para passar dados', exercises: 7 },
-    { name: 'Estado de um componente', exercises: 14 },
-  ];
-  
+  const course = {
+    name: 'Desenvolvimento de aplicação Half Stack',
+    parts: [
+      {
+        name: 'Fundamentos da biblioteca React',
+        exercises: 10
+      },
+      {
+        name: 'Usando props para passar dados',
+        exercises: 7
+      },
+      {
+        name: 'Estado de um componente',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Content parts={course}/>
+      <Total parts={course}/>
     </div>
   )
 }
