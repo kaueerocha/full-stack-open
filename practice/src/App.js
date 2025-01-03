50ececd0b451a39a2f027e1ccdd16e2a51f2e5e5
Kauee Rocha Puertas <kauee-rocha@hotmail.com>
2025-01-03 14:43:19 -0300

    add notes with backend

---
+++ practice/src/App.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => { 
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  ) 
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    // axios.get('http://localhost:3001/notes').then((response) => {
+    //   console.log('promise fulfilled (promessa resolvida)')
+    //   setNotes(response.data)
+    // })
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = 
+    showAll ? notes
+    : notes.filter(note => note.important === true)
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App 
